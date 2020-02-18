5d015ff143c2c34ff0b3fb4372cec97d7223e1c4
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2020-02-18 15:51:41 +0200

    started create structure of the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { DrinksComponent } from './drinks/drinks.component';
+import { DrinkslistComponent } from './drinks/drinkslist/drinkslist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideBarComponent,
+    DrinksComponent,
+    DrinkslistComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/drinks/drinkslist/drinkslist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-drinkslist',
+  templateUrl: './drinkslist.component.html',
+  styleUrls: ['./drinkslist.component.scss']
+})
+export class DrinkslistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

0859628cf2a304fbbca4bec6a3c517fbe934a3ce
AlekseiiKanaiev <alexey.kanaev.ua@gmail.com>
2020-02-19 18:12:51 +0200

    finished with fiter side-bar

--- src/app/_services/get-data.service.ts
+++ src/app/_services/get-data.service.ts
@@ -1,9 +1,48 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { FiltersData } from '../_models/filter.model';
+import { Subject } from 'rxjs';
+import { CocktailsData, CoctailsList, CocktailModel } from '../_models/cocktailsData.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetDataService {
+  obsCocktailsLists: Subject<CoctailsList[]> = new Subject<CoctailsList[]>();
+  cocktailsLists: CoctailsList[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getFilters() {
+    return this.http.get<FiltersData>('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list').pipe(
+      map((data: FiltersData) => data.drinks.map(el => el.strCategory))
+    );
+  }
+
+  requestData(filters: string[]) {
+    this.cocktailsLists.length = 0;
+    filters.map(el => el.replace(' ', '_'));
+    for (let filter of filters) {
+      const list: CoctailsList = {
+        category: '',
+        cocktails: []
+      };
+      this.http.get<CocktailsData>(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${filter}`).pipe(
+        map((data: CocktailsData) => data.drinks)
+      ).subscribe(
+        (data: CocktailModel[]) => {
+          console.log(data);
+          list.category = filter;
+          list.cocktails = data;
+          this.cocktailsLists.push(list);
+          console.log(this.cocktailsLists);
+          if (this.cocktailsLists.length === filters.length) {
+            console.log(1);
+            this.obsCocktailsLists.next(this.cocktailsLists);
+          }
+        }
+      );
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ContentComponent } from './components/content/content.component';
@@ -15,7 +18,10 @@ import { GetDataService } from './_services/get-data.service';
     DrinksListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [GetDataService],
   bootstrap: [AppComponent]
